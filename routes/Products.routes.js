465a9f927ab72ba85cf9db3673c16e254b893a6d
Sherab Pereira <sherabpereira@gmail.com>
2021-09-16 14:55:28 +0200

    FT: filter products by category

--- routes/Products.routes.js
+++ routes/Products.routes.js
@@ -1,87 +1,105 @@
-const router = require("express").Router();
-const multer = require("multer");
-const upload = multer({ dest: "./public/uploads/" });
-const Product = require("../models/Product.model");
-const Category = require("../models/Category.model");
+const router = require('express').Router()
+const multer = require('multer')
+const upload = multer({ dest: './public/uploads/' })
+const Product = require('../models/Product.model')
+const Category = require('../models/Category.model')
 
 //GET List products
-router.get("/", (_, res, next) => {
-  Product.find()
-    .populate("category")
-    .then((products) =>
-      res.render("products/products-list", { products: products })
-    )
-    .catch((err) => next(err));
-});
+router.get('/', async (_, res, next) => {
+  try {
+    const products = await Product.find().populate('category')
+    const categories = await Category.find()
+    res.render('products/products-list', { categories, products })
+  } catch (err) {
+    next(err)
+  }
+})
 
 //GET create product
-router.get("/create", (_, res, next) => {
+router.get('/create', (_, res, next) => {
   Category.find()
     .then((categories) => {
-      res.render("products/new-product", { categories: categories });
+      res.render('products/new-product', { categories: categories })
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 //POST create product
-router.post("/create", upload.single("image"), (req, res, next) => {
-  const { name, price, description, category, brand } = req.body;
+router.post('/create', upload.single('image'), (req, res, next) => {
+  const { name, price, description, category, brand } = req.body
 
   const image = {
     name: req.body.name,
     path: `/uploads/${req.file.filename}`,
     originalName: req.file.originalname,
-  };
+  }
 
-  console.log(image);
+  console.log(image)
 
   Product.create({ name, price, description, category, brand, image })
     .then(async (product) => {
       await Category.findByIdAndUpdate(category, {
         $push: { products: { _id: product._id } },
-      });
+      })
     })
-    .then(res.redirect("/products"))
-    .catch((err) => next(err));
-});
+    .then(res.redirect('/products'))
+    .catch((err) => next(err))
+})
 
 //GET Find product
-router.get("/:id", (req, res, next) => {
-  const { id } = req.params;
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params
   Product.findById(id)
-    .populate("category")
-    .then((product) => res.render("products/product-details", product))
-    .catch((err) => next(err));
-});
+    .populate('category')
+    .then((product) => res.render('products/product-details', product))
+    .catch((err) => next(err))
+})
+
+//GET Find products of category
+router.get('/filter/:id', async (req, res, next) => {
+  const { id } = req.params
+
+  try {
+    const category = await Category.findById(id).populate('products')
+    const categories = await Category.find()
+
+    res.render('products/products-list', {
+      products: category.products,
+      categories,
+    })
+  } catch (err) {
+    next(err)
+  }
+})
 
 //POST delete product
-router.post("/delete/:id", (req, res, next) => {
-  const { id } = req.params;
+router.post('/delete/:id', (req, res, next) => {
+  const { id } = req.params
 
   Product.findByIdAndRemove(id)
-    .then(res.redirect("/products"))
-    .catch((err) => next(err));
-});
+    .then(res.redirect('/products'))
+    .catch((err) => next(err))
+})
 
 //GET Edit product
-router.get("/edit/:id", (req, res, next) => {
-  const { id } = req.params;
+router.get('/edit/:id', (req, res, next) => {
+  const { id } = req.params
 
   Promise.all([Product.findById(id), Category.find()])
     .then(([product, categories]) =>
-      res.render("products/edit-product", { product, categories })
+      res.render('products/edit-product', { product, categories }),
     )
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 //POST edit product
-router.post("/edit/:id", (req, res, next) => {
-  const { id } = req.params;
-  const { name, price, description, category, brand } = req.body;
+router.post('/edit/:id', (req, res, next) => {
+  const { id } = req.params
+  const { name, price, description, category, brand } = req.body
 
   Product.findByIdAndUpdate(id, { name, price, description, category, brand })
     .then(res.redirect(`/products/`))
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
-module.exports = router;
+module.exports = router
--- routes/Shop.routes.js
+++ routes/Shop.routes.js
@@ -1,14 +1,15 @@
-const router = require("express").Router();
+const router = require('express').Router()
 
-const Category = require("../models/Category.model");
-const Product = require("../models/Product.model");
+const Category = require('../models/Category.model')
+const Product = require('../models/Product.model')
 
 //GET Shop products list
-router.get("/", async (req, res, next) => {
-  const categories = await Category.find();
-  const products = await Product.find();
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await Category.find()
+    const products = await Product.find()
+    res.render('shop/product-gallery', { categories, products })
+  } catch (err) {}
+})
 
-  res.render("shop/product-gallery", { categories, products });
-});
-
-module.exports = router;
+module.exports = router
